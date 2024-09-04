b74e27d01f5c623b7696201d18a3bd788d88be30
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-09-03 22:46:47 -0400

    feat: add tests for getting type of player

--- player.test.js
+++ player.test.js
@@ -1,7 +1,12 @@
 import Player from "./player.js";
 
 const player1 = Player();
+const player2 = Player("computer");
 
-it("Player type is computer", () => {
+it("Player type is player with no arguments", () => {
   expect(player1.type).toBe("player");
+});
+
+it("Player type is computer when added as argument", () => {
+  expect(player2.type).toBe("computer");
 });
