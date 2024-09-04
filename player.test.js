3c7bc01d9a25adae0a60c9a051dc33b4eea2b326
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-09-03 23:02:23 -0400

    feat; add tests for if players have own board

--- player.test.js
+++ player.test.js
@@ -10,3 +10,12 @@ it("Player type is player with no arguments", () => {
 it("Player type is computer when added as argument", () => {
   expect(player2.type).toBe("computer");
 });
+
+it("Players have their own board", () => {
+  for (let row = 0; row < player1.board.size; row++) {
+    for (let col = 0; col < player1.board.size; col++) {
+      expect(player1.board.getLayout()[row][col]).toBe(0);
+      expect(player2.board.getLayout()[row][col]).toBe(0);
+    }
+  }
+});
