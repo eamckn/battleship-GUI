7f84781b8459221102336df5514282a2508e2ac3
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-09-03 21:12:57 -0400

    feat: add tests to ship.test.js

---
+++ ship.test.js
@@ -0,0 +1,18 @@
+import Ship from "./ship.js";
+
+const cruiser = Ship(3);
+
+it("Hit count is incremented", () => {
+  cruiser.hit();
+  expect(cruiser.getHitCount()).toBe(1);
+});
+
+it("Sunk status is false on no hits", () => {
+  expect(cruiser.isSunk()).toBe(false);
+});
+
+it("Sunk status is true on hits equal to length", () => {
+  cruiser.hit();
+  cruiser.hit();
+  expect(cruiser.isSunk()).toBe(true);
+});
