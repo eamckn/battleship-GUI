b6a4251476be6656bd0f597c38eaad5c215c4d05
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-09-05 19:59:09 -0400

    feat: add func to create ship divs for dragdrop

--- dom-manip.js
+++ dom-manip.js
@@ -1,6 +1,7 @@
 const main = document.querySelector("div#main");
 const player1BoardDisplay = document.querySelector("div.gameboard.player1");
 const player2BoardDisplay = document.querySelector("div.gameboard.player2");
+const player1ShipsDisplay = document.querySelector("div.ships.player1");
 
 export default function DOM_manip() {
   const renderInitial = function renderBothPlayerBoardsOnShipPlacements(
@@ -32,9 +33,28 @@ export default function DOM_manip() {
     main.appendChild(player2BoardDisplay);
   };
 
+  const initalizeShips = function initalizeShipsToBePlacedOnPlayerOneBoard() {
+    const patrol = document.createElement("div");
+    patrol.className = "ship player1 patrol";
+    const sub = document.createElement("div");
+    sub.className = "ship player1 sub";
+    const destroyer = document.createElement("div");
+    destroyer.className = "ship player1 destroyer";
+    const battleship = document.createElement("div");
+    battleship.className = "ship player1 patbattleshiprol";
+    const carrier = document.createElement("div");
+    carrier.className = "ship player1 carrier";
+
+    player1ShipsDisplay.appendChild(patrol);
+    player1ShipsDisplay.appendChild(sub);
+    player1ShipsDisplay.appendChild(destroyer);
+    player1ShipsDisplay.appendChild(battleship);
+    player1ShipsDisplay.appendChild(carrier);
+  };
+
   const updateSquare = function updateSquareValueAfterAttack(square, value) {
     square.setAttribute("value", value);
   };
 
-  return { renderInitial, updateSquare };
+  return { renderInitial, updateSquare, initalizeShips };
 }
