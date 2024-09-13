9daa78536d47982cd50585b5b22f64be07ecdb7d
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-09-13 15:18:39 -0400

    feat: show rules dialog on name dialog btn click

--- dialog.js
+++ dialog.js
@@ -60,6 +60,21 @@ const enableNameButton = function enableNameButtonToReturnPlayerName() {
     //console.log(val);
     nameDialog.close();
     player1Name = val;
+    showRulesDialog();
     //console.log(player1Name);
   });
 };
+
+const showRulesDialog = function displayDialogDescribingGameRules() {
+  const rulesDialog = document.querySelector("dialog#rules");
+  rulesDialog.showModal();
+  enableRulesButton();
+};
+
+const enableRulesButton =
+  function addEventListenerToRulesDialogButtonToCloseDialog() {
+    const rulesButton = document.querySelector("dialog#rules button");
+    rulesButton.addEventListener("click", () => {
+      document.querySelector("dialog#rules").close();
+    });
+  };
